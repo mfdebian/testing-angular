21de2c499073600da46f4f00b2182fb55d8162fb
Alfredo González <alfredo.arosteguy@gmail.com>
2021-11-16 15:20:24 -0300

    New tests passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,41 +14,46 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Testing App'`, async(() => {
+  it(`should have a title of 'Testing App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app.title).toEqual('Testing App');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it(`should render title in a h1 tag`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
+
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Testing App!');
   }));
 
   it(`should have onClick() function`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    // Check if function is defined
+
     expect(app.onClick).toBeDefined();
   }));
 
   it(`message should be "" before onClick() function call`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    // Check message value before function call
+
     expect(app.message).toMatch("");
   }));
 
   it(`message should change to "Click" after onClick() function call`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     // Calls function
     app.onClick()
+
     // Check message value after function call
     expect(app.message).toMatch("Click");
   }));
